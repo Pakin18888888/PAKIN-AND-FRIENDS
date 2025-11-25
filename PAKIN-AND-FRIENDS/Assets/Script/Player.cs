7a7458ac0c4050fb0f9a7467257c916ddefca063
Pakin18888888 <238475616+takzakung9-hue@users.noreply.github.com>
2025-11-25 08:44:46 +0700

    Add

--- PAKIN-AND-FRIENDS/Assets/Script/Player.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/Player.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IInteractable
 {
     public Inventory inventoryl;
     // public List<Skill> activeSkills;
     public int senity;
     public int stamina;
 
-    public void Interact(){
+    public void Interact(IInteractable obj){
 
     }
     
