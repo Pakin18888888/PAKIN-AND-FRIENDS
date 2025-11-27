61296df213d7cf36ac3af053abe0b7fd8f057f77
bocchiii0019 <jaruwantreechot@gmail.com>
2025-11-27 23:29:06 +0700

    nehao

--- PAKIN-AND-FRIENDS/Assets/Script/Enemy.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/Enemy.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Sta
+    // rt is called before the first frame update
+    public Transform target;
+    private string EnemyID;
+    public float detectionRange = 2f;
+    public float moveSpeed = 3f;
+    public float detectionRadius = 1f;
+    private bool isFollowing = false;
+    public float detectionTime = 2f;
+    private float outofdetectionRangeTimer = 0f;
+    
     void Start()
     {
         
@@ -13,6 +23,32 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance <= detectionRadius)
+        {
+            isFollowing = true;
+        }
+        else
+        {
+            isFollowing = false;
+            outofdetectionRangeTimer += Time.deltaTime;
+        }
         
+        if (isFollowing)
+        {
+            if (distance > detectionRange)
+            {
+            // ทิศทางวิ่งเข้าหา
+            Vector3 direction = (target.position - transform.position).normalized;
+
+            // ขยับตัวละครไปตามทิศทาง
+            transform.position += direction * moveSpeed * Time.deltaTime;
+            }
+        }
+
+        if (outofdetectionRangeTimer >= detectionTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
