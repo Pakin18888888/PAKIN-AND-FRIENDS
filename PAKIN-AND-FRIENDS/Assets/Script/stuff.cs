b2579a97b5d7140526a62622d9a0fa1f1b27a2c0
Pakin18888888 <238475616+takzakung9-hue@users.noreply.github.com>
2025-11-28 05:31:33 +0700

    👍 E

--- PAKIN-AND-FRIENDS/Assets/Script/stuff.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/stuff.cs
@@ -1,37 +1,51 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class stuff : Identity
+public class stuff : MonoBehaviour
 {
-    public TMP_Text interactionTextUI;
-    protected Collider2D _collider;
-    public bool isHide;
+    public TextMeshProUGUI GJ;
+    public GameObject GJ1;
+    public bool pickUpAllowed;
 
-    protected override void SetupIdentity()
+    void Start()
     {
-        interactionTextUI = GetComponentInChildren<TMP_Text>();
-        _collider = GetComponent<Collider2D>();
+        GJ.gameObject.SetActive(false);
+        pickUpAllowed = false;
     }
 
     void Update()
     {
-        if (GetDistancePlayer() > 2f)
-        {
-            interactionTextUI.gameObject.SetActive(false);
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E)){
+
+            bool isActive = !GJ1.activeSelf; 
+            GJ1.SetActive(isActive);
+
+            if (isActive)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
         }
-        else
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
         {
-            interactionTextUI.gameObject.SetActive(true);
+            GJ.gameObject.SetActive(true);
+            pickUpAllowed = true;
         }
     }
 
-    float GetDistancePlayer()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        // คุณต้องแก้ส่วนนี้ให้ถูกตามโปรเจคคุณ
-        // ตัวอย่าง: หา Player จาก tag
-        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
-
-        return Vector3.Distance(transform.position, player.position);
+        if(collision.gameObject.tag == "Player")
+        {
+            GJ.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
     }
 }
