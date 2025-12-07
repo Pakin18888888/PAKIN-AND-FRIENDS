a17aa097487507e80c3b7a42d750000d479f9264
Pakin18888888 <238475616+takzakung9-hue@users.noreply.github.com>
2025-12-08 05:14:51 +0700

    👍

--- PAKIN-AND-FRIENDS/Assets/Script/HomeMenu/StartButton.cs
+++ PAKIN-AND-FRIENDS/Assets/Script/HomeMenu/StartButton.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class StartButton : MonoBehaviour
 {
     public GameObject startButton;
+    
+    void Start()
+    {
+        startButton.SetActive(false);
+    }
 
     public void ShowButton()
     {
         startButton.SetActive(true);
     }
 
-    void Start()
-    {
-        startButton.SetActive(false); // ซ่อนปุ่มก่อน
-    }
+    
 }
 
